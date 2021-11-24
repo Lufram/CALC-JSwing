52cd05f6fba0bea556fc946c4370f45f12837939
Lufram <63366850+Lufram@users.noreply.github.com>
2021-11-24 21:14:04 +0100

    Update Main.java

--- controler/Main.java
+++ controler/Main.java
@@ -3,7 +3,7 @@ package controler;
 import view.MainWindow;
 
 public class Main {
-
+	// Al arrancar crea un objeto que abrira la ventana
 	public static void main(String[] args) {
 		MainWindow myWindoww = new MainWindow();
 		
