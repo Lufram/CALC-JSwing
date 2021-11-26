981c25cbc10fe680586d01775dd777836f7891b1
Lufram <63366850+Lufram@users.noreply.github.com>
2021-11-26 20:37:15 +0100

    Update MainWindow.java

--- view/MainWindow.java
+++ view/MainWindow.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
@@ -30,29 +32,78 @@ public class MainWindow extends JFrame {
 			"0",".","=",
 	};
 	
+	private String[] pycto = {"x", "-", "+"};
+	
 	// Variables
 	public int operator = 0;
 	public double firstNum = 0, secondNum = 0;
 	
 	// Paneles
-	private JPanel panel = new JPanel(new BorderLayout(5,5));
-	private JPanel btnPanel = new JPanel(new GridLayout(5,4,1,1));
+	private JPanel panel = new RoundPanel(new GridBagLayout(), 50, Color.decode("#353535"));
+	
+	private JPanel btnWPanel = new JPanel(new GridBagLayout());
+	private JButton[] wBtns = new JButton[3];
+	
+	public JTextField screen = new JTextField(30);
+	
+	private JPanel btnPanel = new JPanel(new GridBagLayout());
 	private JButton[] btns = new JButton[19];
-	public JTextArea screen = new JTextArea(5,40);
+	
 	
 	// Esto no me acuerdo para que era xDD
-	private JTextField calculatingTf = new JTextField(40);
+	//private JTextField calculatingTf = new JTextField(40);
 	
 	
 	// Constructor
 	public MainWindow() {
+		// Tamaño de la ventana
+		setSize(300,500);
+		// Localizaon de la ventana
+		setLocationRelativeTo(null);
+		// Metodo de cierre
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// Evita que el usuario redimensione la ventana
+		setResizable(false);
+		// Asigna un titulo
+		setTitle("Calculadora");
+		// Iniciamos los componentes
+		initComponents();
+		// Sin decoracion
+		//setUndecorated(true);
+		// Fondo transparente
+		//setBackground(new Color(0.0f,0.0f,0.0f,0.0f));
+		// Visibilidad -- colocar la ultima
+		setVisible(true);
+	}
+	
+
+	private void initComponents() {
 		
-		// asigna las propiedades a los paneles
+		// Asigna las propiedades a los paneles
 		panel.setBackground(Color.decode("#353535"));
-		btnPanel.setBackground(Color.decode("#353535"));
-		screen.setBackground(Color.decode("#353535"));
+		// Color y fuente del texto
 		screen.setForeground(Color.WHITE);
+		screen.setOpaque(false);
 		screen.setFont(new Font("Roboto", Font.BOLD, 25));
+		btnPanel.setOpaque(false);
+		
+		int colCont = 0;
+		int lineCont = 0;
+		
+		for (int i = 0; i < wBtns.length ; i++) {
+			// Crea un boton
+			btns[i] = new JButton(symbols[i]);
+			// Opacidad
+			btns[i].setOpaque(true);
+			
+			btns[i].setFont(new Font("Roboto", Font.ROMAN_BASELINE, 25));
+			// Añade un listener al boton
+			btns[i].addActionListener(new EventManager(this));
+			
+			btns[i].setBackground(Color.decode("#e47608"));
+			// Contenido en blanco
+			btns[i].setForeground(Color.WHITE);
+		}
 		
 		// Bucle que crea un boton por cada simbolo del array
 		for (int i = 0; i < btns.length ; i++) {
@@ -68,7 +119,7 @@ public class MainWindow extends JFrame {
 				// Contenido en blanco
 				btns[i].setForeground(Color.WHITE);
 				
-			}else if (symbols[i] == "÷" | symbols[i] == "x" | symbols[i] == "-" | symbols[i] == "+" ) {
+			}else if (symbols[i] == "÷" | symbols[i] == "x" | symbols[i] == "-" | symbols[i] == "+" | symbols[i] == "=" ) {
 				
 				btns[i].setBackground(Color.decode("#e47608"));
 				// Contenido en blanco
@@ -82,37 +133,54 @@ public class MainWindow extends JFrame {
 			// Añade un listener al boton
 			btns[i].addActionListener(new EventManager(this));
 			
-			// Intento que el boton 0 ocupe 2 espacios
+			// Propiedades comunes	
 			GridBagConstraints c = new GridBagConstraints();
-			c.gridwidth = 2;
-			c.fill = GridBagConstraints.HORIZONTAL;
-			// Añade el boton al panel
-			if(symbols[i] == "0") {
-				btnPanel.add(btns[i],c);
-			}else {
-				btnPanel.add(btns[i]);
+			c.insets = new Insets(0,0,1,1);
+			c.fill = GridBagConstraints.BOTH;
+			c.weightx = 1.0;
+			c.weighty = 1.0;
+			
+			c.gridx = colCont;
+			c.gridy = lineCont;
+			c.gridheight = 1;
+			if (colCont == 0 & lineCont == 4) { 
+				c.gridwidth = 2;
+				colCont++;
+			} else {
+				c.gridwidth = 1;
 			}
+			colCont++;
+			if (colCont > 3) { colCont = 0; lineCont++;}
+			
+			
+			// Añade el boton al panel
+			btnPanel.add(btns[i],c);
+		
 		}
 		
+		GridBagConstraints f = new GridBagConstraints();
+		f.insets = new Insets(0,0,1,1);
+		f.fill = GridBagConstraints.BOTH;
+		f.weightx = 0.2;
+		f.weighty = 0.2;
+		
+		f.gridx = 0;
+		f.gridy = 0;
+		f.gridheight = 1;
+		f.gridwidth = 1;
 		// Asigna la gerarquia de los elementos
-		panel.add(calculatingTf, BorderLayout.SOUTH);
-		panel.add(btnPanel, BorderLayout.CENTER);
-		panel.add(screen, BorderLayout.NORTH);
+		panel.add(btnWPanel, f);
+		f.gridy = 1;
+		f.weighty = 4;
+		panel.add(screen, f);
+		//panel.add(calculatingTf, BorderLayout.SOUTH);
+		f.gridy = 2;
+		f.weighty = 6;
+		panel.add(btnPanel, f);
+		
 		add(panel);
-		// Tamaño de la ventana
-		setSize(300,500);
-		// Localizaon de la ventana
-		setLocationRelativeTo(null);
-		// Metodo de cierre
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		// Evita que el usuario redimensione la ventana
-		setResizable(false);
-		// Asigna un titulo
-		setTitle("Calculadora");
-		// Visibilidad -- colocar la ultima
-		setVisible(true);
+		
 	}
 	
-
 }
 
