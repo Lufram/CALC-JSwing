3e4236db1a2ce2610766b1cde577653abcc4242d
Lufram <63366850+Lufram@users.noreply.github.com>
2021-11-28 01:19:52 +0100

    Update MainWindow.java

--- view/MainWindow.java
+++ view/MainWindow.java
@@ -10,11 +10,14 @@ import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import controler.EventManager;
 
@@ -25,38 +28,37 @@ public class MainWindow extends JFrame {
 	
 	// Simbolos para los botones
 	private String[] symbols = {
-			"AC", "+/-", "%", "÷",
+			"AC", "+/-", "%", "Ã·",
 			"7","8","9","x",
 			"4","5","6","-",
 			"1","2","3","+",
 			"0",".","=",
 	};
 	
+	
 	private String[] pycto = {"x", "-", "+"};
 	
 	// Variables
 	public int operator = 0;
 	public double firstNum = 0, secondNum = 0;
+	public String condicion = "vacio";
 	
 	// Paneles
 	private JPanel panel = new RoundPanel(new GridBagLayout(), 50, Color.decode("#353535"));
 	
-	private JPanel btnWPanel = new JPanel(new GridBagLayout());
-	private JButton[] wBtns = new JButton[3];
+	//private JPanel btnWPanel = new JPanel(new GridBagLayout());
+	//private JButton[] wBtns = new JButton[3];
 	
-	public JTextField screen = new JTextField(30);
+	public JTextField screen = new JTextField();
 	
 	private JPanel btnPanel = new JPanel(new GridBagLayout());
 	private JButton[] btns = new JButton[19];
-	
-	
-	// Esto no me acuerdo para que era xDD
-	//private JTextField calculatingTf = new JTextField(40);
+
 	
 	
 	// Constructor
 	public MainWindow() {
-		// Tamaño de la ventana
+		// TamaÃ±o de la ventana
 		setSize(300,500);
 		// Localizaon de la ventana
 		setLocationRelativeTo(null);
@@ -74,37 +76,53 @@ public class MainWindow extends JFrame {
 		//setBackground(new Color(0.0f,0.0f,0.0f,0.0f));
 		// Visibilidad -- colocar la ultima
 		setVisible(true);
+	
 	}
 	
 
 	private void initComponents() {
-		
+		// Propiedades del panel principal
 		// Asigna las propiedades a los paneles
+		df.setDecimalSeparatorAlwaysShown(false);
 		panel.setBackground(Color.decode("#353535"));
+		// Propiedades del panel donde se escribiran los numero de la calculadora
 		// Color y fuente del texto
 		screen.setForeground(Color.WHITE);
+		// Opacidad
 		screen.setOpaque(false);
-		screen.setFont(new Font("Roboto", Font.BOLD, 25));
+		screen.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+		screen.setHorizontalAlignment(SwingConstants.RIGHT);
+		// Tipografia y tamaÃ±o de letra
+		screen.setFont(new Font("Roboto", Font.BOLD, 40));
+		// Propieades del panel contenedor de botones
+		// Oppcidad
 		btnPanel.setOpaque(false);
 		
-		int colCont = 0;
-		int lineCont = 0;
 		
+		/*
 		for (int i = 0; i < wBtns.length ; i++) {
 			// Crea un boton
-			btns[i] = new JButton(symbols[i]);
+			wBtns[i] = new JButton(pycto[i]);
 			// Opacidad
-			btns[i].setOpaque(true);
-			
-			btns[i].setFont(new Font("Roboto", Font.ROMAN_BASELINE, 25));
-			// Añade un listener al boton
-			btns[i].addActionListener(new EventManager(this));
-			
-			btns[i].setBackground(Color.decode("#e47608"));
+			wBtns[i].setOpaque(true);
+			// Tipografia y tamaÃ±o de letra
+			wBtns[i].setFont(new Font("Roboto", Font.ROMAN_BASELINE, 25));
+			// AÃ±ade un listener al boton
+			wBtns[i].addActionListener(new EventManager(this));
+			// Color de fondo
+			wBtns[i].setBackground(Color.decode("#e47608"));
 			// Contenido en blanco
-			btns[i].setForeground(Color.WHITE);
+			wBtns[i].setForeground(Color.WHITE);
+			wBtns[i].setFocusable(false);
+			
+			btnWPanel.add(wBtns[i]);
 		}
+		*/
 		
+		//Variables que utilizaremos para generar los botones y su colocacion
+				int colCont = 0;
+				int lineCont = 0;
+				
 		// Bucle que crea un boton por cada simbolo del array
 		for (int i = 0; i < btns.length ; i++) {
 			// Crea un boton
@@ -114,13 +132,13 @@ public class MainWindow extends JFrame {
 			// Sin borde
 			btns[i].setBorderPainted(false);
 			if (symbols[i] == "AC" | symbols[i] == "+/-" | symbols[i] == "%" ){
-				
+				// Color de fondo
 				btns[i].setBackground(Color.decode("#949392"));
 				// Contenido en blanco
 				btns[i].setForeground(Color.WHITE);
 				
-			}else if (symbols[i] == "÷" | symbols[i] == "x" | symbols[i] == "-" | symbols[i] == "+" | symbols[i] == "=" ) {
-				
+			}else if (symbols[i] == "Ã·" | symbols[i] == "x" | symbols[i] == "-" | symbols[i] == "+" | symbols[i] == "=" ) {
+				// Color de fondo
 				btns[i].setBackground(Color.decode("#e47608"));
 				// Contenido en blanco
 				btns[i].setForeground(Color.WHITE);
@@ -130,10 +148,12 @@ public class MainWindow extends JFrame {
 				btns[i].setForeground(Color.BLACK);
 			}
 			btns[i].setFont(new Font("Roboto", Font.ROMAN_BASELINE, 25));
-			// Añade un listener al boton
+			// AÃ±ade un listener al boton
 			btns[i].addActionListener(new EventManager(this));
+			btns[i].setFocusable(false);
 			
-			// Propiedades comunes	
+			
+			// Propiedades comunes	para gid bag
 			GridBagConstraints c = new GridBagConstraints();
 			c.insets = new Insets(0,0,1,1);
 			c.fill = GridBagConstraints.BOTH;
@@ -143,6 +163,8 @@ public class MainWindow extends JFrame {
 			c.gridx = colCont;
 			c.gridy = lineCont;
 			c.gridheight = 1;
+			
+			// TamaÃ±o mas grande para el boton del 0
 			if (colCont == 0 & lineCont == 4) { 
 				c.gridwidth = 2;
 				colCont++;
@@ -150,10 +172,10 @@ public class MainWindow extends JFrame {
 				c.gridwidth = 1;
 			}
 			colCont++;
+			
+			//Reinicia a 0 la columna y salta de linea
 			if (colCont > 3) { colCont = 0; lineCont++;}
-			
-			
-			// Añade el boton al panel
+			// AÃ±ade el boton al panel
 			btnPanel.add(btns[i],c);
 		
 		}
@@ -166,16 +188,12 @@ public class MainWindow extends JFrame {
 		
 		f.gridx = 0;
 		f.gridy = 0;
-		f.gridheight = 1;
-		f.gridwidth = 1;
 		// Asigna la gerarquia de los elementos
-		panel.add(btnWPanel, f);
-		f.gridy = 1;
 		f.weighty = 4;
 		panel.add(screen, f);
 		//panel.add(calculatingTf, BorderLayout.SOUTH);
-		f.gridy = 2;
-		f.weighty = 6;
+		f.gridy = 1;
+		f.weighty = 14;
 		panel.add(btnPanel, f);
 		
 		add(panel);
